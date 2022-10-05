aac4d94c46a8579447b0b01c0a8a487afbce3e3d
Graham Bygrave <graham@bygrave.org>
2022-10-05 11:07:03 +0100

    Refactored model (JSONable) objects to common package. Closes #2.

--- src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -1,10 +1,10 @@
 package guru.sfg.beer.inventory.service.services;
 
-import guru.sfg.beer.common.events.NewInventoryEvent;
+import guru.sfg.brewery.model.events.NewInventoryEvent;
 import guru.sfg.beer.inventory.service.config.JmsConfig;
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
-import guru.sfg.beer.inventory.service.web.model.BeerDto;
+import guru.sfg.brewery.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
--- src/main/java/guru/sfg/brewery/model/BeerStyleEnum.java
+++ src/main/java/guru/sfg/brewery/model/BeerStyleEnum.java
@@ -1,4 +1,4 @@
-package guru.sfg.beer.inventory.service.web.model;
+package guru.sfg.brewery.model;
 
 public enum BeerStyleEnum {
     LAGER, PILSNER, SOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
