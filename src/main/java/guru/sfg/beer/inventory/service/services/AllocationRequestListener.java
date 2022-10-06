c59a2aa1bb85a088d984fb909b67c3cd9c85e416
Graham Bygrave <graham@bygrave.org>
2022-10-06 10:26:53 +0100

    Changes to bring in line with official solution. Closes #6.

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationRequestListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationRequestListener.java
@@ -8,30 +8,32 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.stereotype.Component;
 
 import static guru.sfg.beer.inventory.service.config.JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE;
 
-@Service
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class AllocationRequestListener {
     private final AllocationService allocationService;
     private final JmsTemplate jmsTemplate;
 
-    @Transactional
     @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
     public void listen(AllocateOrderRequest event) {
         BeerOrderDto beerOrderDto = event.getBeerOrderDto();
-        Boolean fullyAllocated = allocationService.allocateOrder(beerOrderDto);
 
-        log.debug("Order " + beerOrderDto.getId() + (fullyAllocated ? "is" : "is not") + " fully allocated.");
+        Boolean fullyAllocated = false, allocationError = false;
+        try {
+            fullyAllocated = allocationService.allocateOrder(beerOrderDto);
+            log.debug("Order " + beerOrderDto.getId() + (fullyAllocated ? "is" : "is not") + " fully allocated.");
+        } catch (Exception ex) {
+            log.error("Allocation failed for Order Id: " + beerOrderDto.getId(), ex);
+            allocationError = true;
+        }
 
-        // TODO: AllocationError currently hardcoded to false.  This isn't something we generate right now.
         AllocateOrderResponse allocateOrderResponse = new AllocateOrderResponse(beerOrderDto,
-                                                                                false,
+                                                                                allocationError,
                                                                                 fullyAllocated);
         jmsTemplate.convertAndSend(ALLOCATE_ORDER_RESPONSE_QUEUE, allocateOrderResponse);
     }
