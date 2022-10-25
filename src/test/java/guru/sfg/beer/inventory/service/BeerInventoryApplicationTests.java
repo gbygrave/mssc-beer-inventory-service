021e1fc26e8e1b06307cb9caa6f4253038d6d92b
Graham Bygrave <graham@bygrave.org>
2022-10-25 10:51:29 +0100

    Switch off client access to Eureka / Zipkin during tests.

--- src/test/java/guru/sfg/beer/inventory/service/BeerInventoryApplicationTests.java
+++ src/test/java/guru/sfg/beer/inventory/service/BeerInventoryApplicationTests.java
@@ -2,8 +2,10 @@ package guru.sfg.beer.inventory.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class BeerInventoryApplicationTests {
 
     @Test
